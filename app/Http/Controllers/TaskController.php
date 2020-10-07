af31478e3a484c002cb04317349c3a88f5b196e3
nurzhand81 <nurzhan.duisenov@gmail.com>
2020-10-07 16:54:32 -0700

    Commit task to github

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\task;
+use Illuminate\Http\Request;
+//use App\Http\Controllers\TaskController;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return task::latest()->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required'
+        ],
+            [
+                'title.required' => 'Task input field is required!'
+            ]
+        );
+        task::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $task = task::findOrFail($id);
+        $task->update($request->all());
+        $task->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = task::findOrFail($id);
+        $task->delete();
+        return task::latest()->get();
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+    <div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+        <h1 class="text-white display-4">VueJs Tasks App</h1>
+        <task-component></task-component>
+    </div>
+@endsection
